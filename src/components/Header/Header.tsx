5f5c484ec1c0e007beefc24d38da2c6291659489
sinelnikov-web <sinelnikov-web@mail.ru>
2021-08-02 23:06:39 +0600

    fix timer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -21,7 +21,7 @@ const Header: React.FC<HeaderProps> = ({score, time, startGame}) => {
             <Container>
                 <HeaderInner>
                     <Time>{minutes.toString().length == 2 ? minutes : `0${minutes}`}:{seconds.toString().length == 2 ? seconds : `0${seconds}`}</Time>
-                    {score !== Infinity ? <Score>{score}/<span>16</span></Score> : '00:00'}
+                    {time !== Infinity ? <Score>{score}/<span>16</span></Score> : '00:00'}
                     <Button onClick={startGame}>Start</Button>
                 </HeaderInner>
             </Container>
